088e2c9dc7163a656b9a040e7f104630aa3b5a86
jashwanth <jashwanth@sameersolutions.com>
2023-03-08 18:18:04 +0530

    setup 1

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import '../Topbar/index.scss'
 import linkedinlogo from '../../../assets/linkdinlogo.png'
+import { AiFillHome, AiOutlineUserSwitch, AiOutlineSearch, AiOutlineMessage, AiOutlineBell, } from 'react-icons/ai';
+import {BsBriefcase} from "react-icons/bs"
+
+
+
 const Topbar = () => {
   return (
       <div className='topbar'>
-        <img src={linkedinlogo} />
           
-          Topbar</div>
+          <img src={linkedinlogo} />
+          <div className='react-icons'>
+          <AiOutlineSearch size={25} className='react-icon' />
+          <AiFillHome size={25} className='react-icon'/>
+              <AiOutlineUserSwitch size={25} className='react-icon'/>
+              <AiOutlineMessage size={25} className='react-icon'/>    
+              <BsBriefcase size={25} className='react-icon'/>
+            <AiOutlineBell size={25} className='react-icon'/>  
+              </div>
+          
+          </div>
   )
 }
 
